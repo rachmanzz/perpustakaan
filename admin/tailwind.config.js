c57ad7763dc87d789eb22e272fe5d0182086b375
muhammad abdurrahman <rachman.sd@gmail.com>
2022-07-30 20:43:01 +0700

    update tailwind config

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -13,7 +13,8 @@ module.exports = {
         'mobile-nav': '0px -2px 2px 1px rgba(251,191,36,0.3)',
       },
       backgroundImage: {
-        'focus-off': 'radial-gradient(50% 50% at 50% 50%, #F7EEDE 0%, rgba(247, 238, 222, 0) 100%)'
+        'focus-off': 'radial-gradient(50% 50% at 50% 50%, #F7EEDE 0%, rgba(247, 238, 222, 0) 100%)',
+        'focus-off-dark': 'radial-gradient(50% 50% at 50% 50%, rgba(255, 201, 120, 0.95) 0%, rgba(255, 201, 120, 0.534375) 32.81%, rgba(255, 201, 120, 0.484666) 39.06%, rgba(255, 201, 120, 0) 100%)',
       }
     },
   },
