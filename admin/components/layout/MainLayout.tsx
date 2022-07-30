9f109e059fc759e8fa4c773798fbf5a87f81bac0
muhammad abdurrahman <rachman.sd@gmail.com>
2022-07-30 12:00:36 +0700

    update layout

--- admin/components/layout/MainLayout.tsx
+++ admin/components/layout/MainLayout.tsx
@@ -1,3 +1,31 @@
-export default function MainLayout() {
-    
+import { useEffect, useState } from "react";
+
+
+type componentType = { children: React.ReactNode }
+
+
+const MobileVersion = ({children}: componentType) => {
+    return (
+        <div className="h-screen w-full relative">
+            <div className="absolute bg-[#FFC978] h-16 shadow w-full bottom-0 z-50">
+                helo
+            </div>
+            unu
+        </div>
+    )
+}
+
+const DestopVersion = ({children}: componentType) => {
+    return (<div>is web</div>)
+}
+
+export default function MainLayout({children}: componentType) {
+    const [screenWidth, setScreenWidth] = useState(0);
+    const isMobile = screenWidth < 768;
+    useEffect(() => {
+        setScreenWidth(window.innerWidth);
+        window.addEventListener("resize", () => setScreenWidth(window.innerWidth));
+    }, []);
+
+    return screenWidth === 0 ? <></>: isMobile ? <MobileVersion>{children}</MobileVersion> : <DestopVersion>{children}</DestopVersion>;
 }
--- admin/pages/index.tsx
+++ admin/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import MainLayout from '../components/layout/MainLayout'
 import styles from '../styles/Home.module.css'
+import { NextPageWithLayout } from './_app'
 
-const Home: NextPage = () => {
+const Home: NextPageWithLayout = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -67,6 +69,10 @@ const Home: NextPage = () => {
       </footer>
     </div>
   )
+}
+
+Home.getLayout = function (page) {
+  return (<MainLayout>{page}</MainLayout>)
 }
 
 export default Home
