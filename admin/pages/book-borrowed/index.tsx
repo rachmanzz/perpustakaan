786802991749d4f03e8b9f5ec0eda052e9571213
muhammad abdurrahman <rachman.sd@gmail.com>
2022-07-31 18:35:52 +0700

    input text search borrowed book

--- admin/pages/book-borrowed/index.tsx
+++ admin/pages/book-borrowed/index.tsx
@@ -1,4 +1,7 @@
+import { useRouter } from 'next/router'
+import BookSearch from '../../components/icon/BookSearch'
 import MainLayout from '../../components/layout/MainLayout'
+import useLanguage from '../../i18n'
 import { NextPageWithLayout } from '../_app'
 
 const BorrowedBook: NextPageWithLayout = () => {
@@ -10,7 +13,22 @@ const BorrowedBook: NextPageWithLayout = () => {
 }
 
 BorrowedBook.getLayout = function (page) {
-  return (<MainLayout>{page}</MainLayout>)
+  const {locale} = useRouter()
+  const lang = useLanguage(locale!!)
+  const MobileInput = () => (
+    <div className="relative">
+        <label htmlFor="email-address" className="sr-only">{lang('search_book')}</label>
+        <span className="absolute inset-y-0 left-0 z-10 flex items-center">
+            <span className="py-2 pl-1">
+                <BookSearch size={24} />
+            </span>
+        </span>
+        <input id="email-address" name="search" type="text" autoComplete="off" placeholder='' required className="appearance-none rounded pl-8 pr-2 placeholder:text-[#F39508]/[50%] font-sans relative w-full  py-2 bg-[#FFE6C0] placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none sm:text-sm" />
+    </div>
+  ) 
+  return (<MainLayout specific={ {
+    mobileOnly: <MobileInput />
+  } }>{page}</MainLayout>)
 }
 
 export default BorrowedBook
