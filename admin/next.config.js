8ab403ffadf96e86c5357f6cac5225697fd39e0f
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-01 14:21:04 +0700

    update base path

--- admin/next.config.js
+++ admin/next.config.js
@@ -1,15 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '/admin',
-  output: 'standalone',
-  i18n: {
-    locales: ['en', 'id'],
-    defaultLocale: 'id',
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
-  }
-}
 
-module.exports = nextConfig
+if (typeof process.env.BASE_PATH !== 'undefined' && process.env.BASE_PATH !== 'none') {
+  const nextConfig = {
+    basePath: '/' + process.env.BASE_PATH,
+    output: 'standalone',
+    i18n: {
+      locales: ['en', 'id'],
+      defaultLocale: 'id',
+    },
+    webpack: (config) => {
+      config.resolve.fallback = { fs: false };
+      return config;
+    }
+  }
+  module.exports = nextConfig
+} else {
+  const nextConfig = {
+    output: 'standalone',
+    i18n: {
+      locales: ['en', 'id'],
+      defaultLocale: 'id',
+    },
+    webpack: (config) => {
+      config.resolve.fallback = { fs: false };
+      return config;
+    }
+  }
+  module.exports = nextConfig
+}
