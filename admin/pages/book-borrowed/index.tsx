45bd74b0690e23c042a39c04fe437425166199b0
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-01 13:59:48 +0700

    edit search form

--- admin/pages/book-borrowed/index.tsx
+++ admin/pages/book-borrowed/index.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import AccountClockOutline from '../../components/icon/AccountClockOutline'
 import BookSearch from '../../components/icon/BookSearch'
 import MainLayout from '../../components/layout/MainLayout'
 import useLanguage from '../../i18n'
@@ -20,10 +21,10 @@ BorrowedBook.getLayout = function (page) {
         <label htmlFor="search-book" className="sr-only">{lang('search_book')}</label>
         <span className="absolute inset-y-0 left-0 z-10 flex items-center">
             <span className="py-2 pl-1">
-                <BookSearch size={24} />
+                <AccountClockOutline size={24} />
             </span>
         </span>
-        <input id="search-book" name="search" type="text" autoComplete="off" placeholder={lang('search_book_identifier')} required className="appearance-none rounded pl-8 pr-2 placeholder:text-[#F39508]/[50%] font-sans relative w-full  py-2 bg-[#FFE6C0] placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none sm:text-sm" />
+        <input id="search-book" name="search" type="text" autoComplete="off" placeholder={lang('find_book_borrower')} required className="appearance-none rounded pl-8 pr-2 placeholder:text-[#F39508]/[50%] font-sans relative w-full  py-2 bg-[#FFE6C0] placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none sm:text-sm" />
     </div>
   )
   const DesktopInput = () => (
@@ -31,10 +32,10 @@ BorrowedBook.getLayout = function (page) {
         <label htmlFor="search-book" className="sr-only">{lang('search_book')}</label>
         <span className="absolute inset-y-0 left-0 z-10 flex items-center">
             <span className="py-2 pl-1">
-                <BookSearch size={24} />
+                <AccountClockOutline size={24} />
             </span>
         </span>
-        <input id="search-book" name="search" type="text" autoComplete="off" placeholder={lang('search_book_identifier')} required className="appearance-none rounded pl-8 pr-2 w-full lg:w-1/2 placeholder:text-[#F39508]/[50%] font-sans relative  py-2 bg-[#FFE6C0] rounded-t-md focus:outline-none sm:text-sm" />
+        <input id="search-book" name="search" type="text" autoComplete="off" placeholder={lang('find_book_borrower')} required className="appearance-none rounded pl-8 pr-2 w-full lg:w-1/2 placeholder:text-[#F39508]/[50%] font-sans relative  py-2 bg-[#FFE6C0] rounded-t-md focus:outline-none sm:text-sm" />
     </div>
   ) 
   return (<MainLayout specific={ {
