1feda6da6a7a898658fe1f350b9250ff01d91e27
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-01 14:35:35 +0700

    remove language from function

--- admin/pages/book-record/index.tsx
+++ admin/pages/book-record/index.tsx
@@ -16,13 +16,13 @@ BookRecord.getLayout = function (page) {
   const lang = useLanguage('en')
   const MobileInput = () => (
     <div className="relative">
-        <label htmlFor="search-book" className="sr-only">{lang('search_book')}</label>
+        <label htmlFor="search-book" className="sr-only">find book</label>
         <span className="absolute inset-y-0 left-0 z-10 flex items-center">
             <span className="py-2 pl-1">
                 <BookSearch size={24} />
             </span>
         </span>
-        <input id="search-book" name="search" type="text" autoComplete="off" placeholder={lang('search_book_by_book_identifier')} required className="appearance-none rounded pl-8 pr-2 placeholder:text-[#F39508]/[50%] font-roboto-opensans placeholder:font-roboto-opensans relative w-full  py-2 bg-[#FFE6C0] placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none sm:text-sm" />
+        <input id="search-book" name="search" type="text" autoComplete="off" placeholder={"find book"} required className="appearance-none rounded pl-8 pr-2 placeholder:text-[#F39508]/[50%] font-roboto-opensans placeholder:font-roboto-opensans relative w-full  py-2 bg-[#FFE6C0] placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none sm:text-sm" />
     </div>
   )
   const DesktopInput = () => (
@@ -33,7 +33,7 @@ BookRecord.getLayout = function (page) {
                 <BookSearch size={24} />
             </span>
         </span>
-        <input id="search-book" name="search" type="text" autoComplete="off" placeholder={lang('search_book_by_book_identifier')} required className="appearance-none rounded pl-8 pr-2 w-full lg:w-1/2 placeholder:text-[#F39508]/[50%] font-roboto-opensans placeholder:font-roboto-opensans relative  py-2 bg-[#FFE6C0] rounded-t-md focus:outline-none sm:text-sm" />
+        <input id="search-book" name="search" type="text" autoComplete="off" placeholder={"find book"} required className="appearance-none rounded pl-8 pr-2 w-full lg:w-1/2 placeholder:text-[#F39508]/[50%] font-roboto-opensans placeholder:font-roboto-opensans relative  py-2 bg-[#FFE6C0] rounded-t-md focus:outline-none sm:text-sm" />
     </div>
   ) 
   return (<MainLayout specific={ {
