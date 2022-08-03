899e88d1148e1a8536bc723ac8fffa61ef5ed8f5
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-03 12:18:07 +0700

    fix element atribute

--- admin/components/items/BreadCrumb.tsx
+++ admin/components/items/BreadCrumb.tsx
@@ -16,7 +16,7 @@ export default function BreadCrumb({ items, forceLowerCase = true, activeClass, 
                             return (
                                 <li key={index} className="inline-flex items-center">
                                     <span className={["inline-flex items-center", activeClass ? activeClass : 'text-sm font-medium text-gray-700 hover:text-gray-900'].join(' ')}>
-                                        <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                                        <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path  fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                                         
                                         {item.icon && <span className="mx-2">{item.icon()}</span>}
                                         {typeof item.item === 'string' ? (forceLowerCase ? item.item.toLowerCase() : item.item) : item.item}
@@ -28,7 +28,7 @@ export default function BreadCrumb({ items, forceLowerCase = true, activeClass, 
                             <li key={index} className="inline-flex items-center">
                                 <Link href={item.href}>
                                     <a className={['inline-flex items-center', className ? className : 'text-sm font-medium text-gray-700 hover:text-gray-900'].join(" ")}>
-                                        <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                                        <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                                         {item.icon && <span className="mx-2">{item.icon()}</span>}
                                         {typeof item.item === 'string' ? (forceLowerCase ? item.item.toLowerCase() : item.item) : item.item}
                                     </a>
