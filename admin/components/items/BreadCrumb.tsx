f124fbec5e5e36a8d5382a1a66d2b258c54874eb
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-03 12:15:11 +0700

    set missing key

--- admin/components/items/BreadCrumb.tsx
+++ admin/components/items/BreadCrumb.tsx
@@ -14,7 +14,7 @@ export default function BreadCrumb({ items, forceLowerCase = true, activeClass, 
                     if (index > 0) {
                         if ((index + 1) == items.length) {
                             return (
-                                <li className="inline-flex items-center">
+                                <li key={index} className="inline-flex items-center">
                                     <span className={["inline-flex items-center", activeClass ? activeClass : 'text-sm font-medium text-gray-700 hover:text-gray-900'].join(' ')}>
                                         <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                                         
@@ -25,7 +25,7 @@ export default function BreadCrumb({ items, forceLowerCase = true, activeClass, 
                             )
                         }
                         return (
-                            <li className="inline-flex items-center">
+                            <li key={index} className="inline-flex items-center">
                                 <Link href={item.href}>
                                     <a className={['inline-flex items-center', className ? className : 'text-sm font-medium text-gray-700 hover:text-gray-900'].join(" ")}>
                                         <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
@@ -38,7 +38,7 @@ export default function BreadCrumb({ items, forceLowerCase = true, activeClass, 
                     }
                     if ((index + 1) == items.length) {
                         return (
-                            <li className="inline-flex items-center">
+                            <li key={index} className="inline-flex items-center">
                                 <span className={["inline-flex items-center", activeClass ? activeClass : 'text-sm font-medium text-gray-700 hover:text-gray-900'].join(' ')}>
                                     {item.icon && <span className="mr-2">{item.icon()}</span>}
                                     {typeof item.item === 'string' ? (forceLowerCase ? item.item.toLowerCase() : item.item) : item.item}
@@ -48,7 +48,7 @@ export default function BreadCrumb({ items, forceLowerCase = true, activeClass, 
                     } 
 
                     return (
-                        <li className="inline-flex items-center">
+                        <li key={index} className="inline-flex items-center">
                             <Link href={item.href}>
                                 <a  className={['inline-flex items-center', className ? className : 'text-sm font-medium text-gray-700 hover:text-gray-900'].join(" ")}>
                                     {item.icon && <span className="mr-2">{item.icon()}</span>}
