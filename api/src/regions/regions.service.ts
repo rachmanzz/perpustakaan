e1a6270ddafaaf7f6771a31c1f7a924525f7d69e
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-13 10:56:21 +0700

    refactor(services): :construction: Inject region repository

--- api/src/regions/regions.service.ts
+++ api/src/regions/regions.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
 import { CreateRegionDto } from './dto/create-region.dto';
 import { UpdateRegionDto } from './dto/update-region.dto';
+import { Region } from './entities/region.entity';
 
 @Injectable()
 export class RegionsService {
+  constructor(
+    @Inject("REGION_REPOSITORY") private readonly regionRepository: Repository<Region>,
+  ) {}
   create(createRegionDto: CreateRegionDto) {
     return 'This action adds a new region';
   }
