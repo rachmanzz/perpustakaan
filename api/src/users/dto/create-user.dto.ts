e4c5480c3d178ee79d308eb2b78dd25d8d70d374
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-15 20:35:27 +0700

    feat(dto): Add dto property for create user

--- api/src/users/dto/create-user.dto.ts
+++ api/src/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+export class CreateUserDto {
+    @ApiProperty()
+    name: string
+
+    @ApiProperty({required: false})
+    username?: string
+
+    @ApiProperty()
+    email: string
+
+    @ApiProperty({required: false})
+    password?: string
+
+    @ApiProperty({default: false})
+    verified?: boolean
+}
