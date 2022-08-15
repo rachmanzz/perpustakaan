c081aa2d213800fe87656f3779885d89131dcf3f
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-15 21:10:51 +0700

    feat: Add feature user register

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, VERSION_NEUTRAL } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { User, UserType } from 'src/users/entities/user.entity';
 import { AuthService } from './auth.service';
 
-@Controller('auth')
+@Controller({ path: 'auth', version: VERSION_NEUTRAL })
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+
+  @Post('register')
+  async register(@Body() dto: CreateUserDto): Promise<User> {
+    try {
+      const user = await this.authService.register(UserType.VISITOR,dto);
+      return user;
+    } catch (error) {
+      return error;
+    }
+  }
 }
--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UserType } from 'src/users/entities/user.entity';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly usersService: UsersService,
+    ){}
+
+    async register(type: UserType = UserType.VISITOR, dto: CreateUserDto) {
+        return await this.usersService.create(type, dto)
+    }
+}
