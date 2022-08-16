6a6d763b62f97a510f97c11865fb0ddcc521224b
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-16 08:00:49 +0700

    test: Add create property at RepositoryMock util

--- api/src/test-utils/repository-mock.utils.ts
+++ api/src/test-utils/repository-mock.utils.ts
@@ -1,10 +1,12 @@
 export type EntityRepositoryType<R, T> = {
+    createResolve?: (...arg: T[]) => R,
     saveResolve?: (...arg:T[]) => R,
     countResolve?: (()=> number) | number
 }
 
 const RepositoryMock = <R, T>(arg: EntityRepositoryType<R, T>|null) => {
     return {
+        create: jest.fn().mockImplementation(async (...arg_opt: T[]) => Promise.resolve(arg?.createResolve?.(...arg_opt) ?? arg_opt)),
         save: jest.fn().mockImplementation((...arg_opt: T[]) => Promise.resolve(arg && typeof arg.saveResolve !== 'undefined' ? arg.saveResolve(...arg_opt): arg_opt)),
         count: jest.fn().mockImplementation(() => Promise.resolve(typeof arg.countResolve !== 'undefined' && typeof arg.countResolve === 'number' ? arg.countResolve : typeof arg.countResolve === 'function' ? arg.countResolve() : 0))
 
