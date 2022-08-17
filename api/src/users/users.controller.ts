392a73ad279bd28f7fe6ca973f64dd26e04d99ce
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-17 20:42:41 +0700

    fix: Fixed version on user controller

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, VERSION_NEUTRAL } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('users')
+@Controller({path: 'users', version: VERSION_NEUTRAL})
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
