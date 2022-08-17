e6d3f6bbb496c66dddc5248479b4392d2584c291
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-17 20:41:37 +0700

    fix: Mapping path module import

--- api/src/auth/auth.service.spec.ts
+++ api/src/auth/auth.service.spec.ts
@@ -1,8 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from '../users/users.service';
-import { userProviders } from '../users/user.providers.mock';
+import { userProviders } from '@apps/users/user.providers.mock';
 import { AuthService } from './auth.service';
-import { getRepositoryToken } from '@nestjs/typeorm';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { LocalStrategy } from './local.strategy';
 import { JwtStrategy } from './jwt.strategy';
