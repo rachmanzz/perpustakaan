e06965801aefcdb7b69ccb05e96e8e509cfd8dc3
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-18 14:51:44 +0700

    test: Rename mocked variable to correct name

--- api/src/users/user.providers.mock.ts
+++ api/src/users/user.providers.mock.ts
@@ -1,11 +1,13 @@
 import RepositoryMock from "../common/test-utils/repository-mock.utils";
 
-const mockedRegionRepository = RepositoryMock({
+const mockedUserRepository = RepositoryMock({
     countResolve: Math.floor(Math.random() * 100) + 1
 })
+
 export const userProviders = [
     {
         provide: 'USER_REPOSITORY',
-        useValue: mockedRegionRepository,
-    }
+        useValue: mockedUserRepository,
+    },
+
 ];
