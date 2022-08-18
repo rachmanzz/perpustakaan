f83f0f1eff31f31f713c9a5f6cda8f15a22c767f
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-18 14:54:20 +0700

    test: Add missing mock provider

--- api/src/users/user.providers.mock.ts
+++ api/src/users/user.providers.mock.ts
@@ -3,11 +3,21 @@ import RepositoryMock from "../common/test-utils/repository-mock.utils";
 const mockedUserRepository = RepositoryMock({
     countResolve: Math.floor(Math.random() * 100) + 1
 })
+const mockedCardRepository = RepositoryMock({})
+const mockedProfileRepository = RepositoryMock({})
 
 export const userProviders = [
     {
         provide: 'USER_REPOSITORY',
         useValue: mockedUserRepository,
     },
+    {
+        provide: 'CARD_REPOSITORY',
+        useValue: mockedCardRepository,
+    },
+    {
+        provide: 'PROFILE_REPOSITORY',
+        useValue: mockedProfileRepository,
+    }
 
 ];
