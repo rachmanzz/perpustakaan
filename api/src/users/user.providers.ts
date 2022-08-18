b6a15392385d4d36a20fdbaa76889f2c4fdf2401
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-18 14:55:02 +0700

    feat: Create repository for Profile and Card

--- api/src/users/user.providers.ts
+++ api/src/users/user.providers.ts
@@ -1,11 +1,23 @@
 import dataSource from 'src/configs/typeorm.config';
 import { DataSource } from 'typeorm';
+import { Card } from './entities/card.entity';
+import { Profile } from './entities/profile.entity';
 import { User } from './entities/user.entity';
 
 export const userProviders = [
     {
         provide: 'USER_REPOSITORY',
         useFactory: (dataSource: DataSource) => dataSource.getRepository(User),
+        inject: ['DATA_SOURCE']
+    },
+    {
+        provide: 'CARD_REPOSITORY',
+        useFactory: (dataSource: DataSource) => dataSource.getRepository(Card),
+        inject: ['DATA_SOURCE']
+    },
+    {
+        provide: 'PROFILE_REPOSITORY',
+        useFactory: (dataSource: DataSource) => dataSource.getRepository(Profile),
         inject: ['DATA_SOURCE']
     }
 ];
