276eadc511a2a9a4197866b55341028e2c07ff18
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-19 11:56:04 +0700

    feat: Add Address repository

--- api/src/regions/region.providers.ts
+++ api/src/regions/region.providers.ts
@@ -1,10 +1,16 @@
 import { DataSource } from 'typeorm';
+import { Address } from './entities/address.entity';
 import { Region } from './entities/region.entity';
 
 export const regionProviders = [
     {
         provide: 'REGION_REPOSITORY',
         useFactory: (dataSource: DataSource) => dataSource.getRepository(Region),
+        inject: ['DATA_SOURCE']
+    },
+    {
+        provide: 'ADDRESS_REPOSITORY',
+        useFactory: (dataSource: DataSource) => dataSource.getRepository(Address),
         inject: ['DATA_SOURCE']
     }
 ];
