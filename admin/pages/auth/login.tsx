90481d98e9825bb1da290ce74890caf3d0273bda
muhammad abdurrahman <rachman.sd@gmail.com>
2022-09-09 10:44:18 +0700

    feat: Add function on submit login button

--- admin/pages/auth/login.tsx
+++ admin/pages/auth/login.tsx
@@ -2,13 +2,53 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import AccountCircleOutline from "../../components/icon/AccountCircleOutline";
+import GoogleCircle from "../../components/icon/GoogleCircel";
 import ShieldKeyOutline from "../../components/icon/ShieldKeyOutline";
+import { useAlert } from "../../components/providers/WrapperProvider";
 import Reading from "../../components/svg-img/Reading"; 
 import useLanguage from "../../i18n";
+import useApi from "../../libraries/api";
+
 
 export default function Login() {
-    const {locale, locales} = useRouter()
-    const lang = useLanguage(locale||'en')
+    const {locale, locales, push: routePush} = useRouter();
+    const lang = useLanguage(locale||'en');
+    const alert = useAlert()
+
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [loading, setLoading] = React.useState(false);
+
+    const api = useApi();
+
+    const onChange = (dispatch: React.Dispatch<React.SetStateAction<string>>) => (state: React.ChangeEvent<HTMLInputElement>) => {
+        if (typeof state.target.value === "undefined") return
+        dispatch(state.target.value as string)
+    }
+
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        // if email and or password is empty
+        if (email === "" || password === "") {
+            return
+        }
+
+        // if email is not email format
+        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+            return
+        }
+
+        // if password is less than 6 character
+        if (password.length < 6) {
+            return
+        }
+        setLoading(true)
+        
+
+
+    }
+
+
     return (
         <div className="min-h-full flex items-center justify-center pt-[10%] px-4 sm:px-6 lg:px-8">
             <div className="max-w-3xl w-full  space-y-8">
@@ -22,7 +62,7 @@ export default function Login() {
                                 <div>
                                     <h2 className="mt-4 text-center text-2xl font-extrabold text-gray-900 font-jakarta-sans">{lang('title_enter_account', 'unique_sentence')}</h2>
                                 </div>
-                                <form className="mt-8 space-y-6">
+                                <form onSubmit={onSubmit}  className="mt-8 space-y-6">
                                     <div className="mx-5">
                                         <div className="m-2 relative">
                                             <label htmlFor="email-address" className="sr-only">Email address</label>
@@ -31,7 +71,8 @@ export default function Login() {
                                                     <AccountCircleOutline size={32} />
                                                 </span>
                                             </span>
-                                            <input id="email-address" name="email" type="email" autoComplete="off" required className="appearance-none rounded pl-11 pr-3 placeholder:text-[#F39508]/[50%] font-sans relative w-full  py-4 bg-[#FFE6C0] placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none sm:text-sm" placeholder={lang('email_address')} />
+                                            {/* pattern email regex */}
+                                            <input id="email-address" onChange={onChange(setEmail)} value={email} name="email" type="email" autoComplete="off" required className="appearance-none rounded pl-11 pr-3 placeholder:text-[#F39508]/[50%] font-sans relative w-full  py-4 bg-[#FFE6C0] placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none sm:text-sm" placeholder={lang('email_address')} />
                                         </div>
                                         <div className="m-2 relative">
                                             <label htmlFor="password-field" className="sr-only">Password</label>
@@ -40,7 +81,7 @@ export default function Login() {
                                                     <ShieldKeyOutline size={32} />
                                                 </span>
                                             </span>
-                                            <input id="password-field" name="password" type="password" autoComplete="off" required className="appearance-none rounded pl-11 placeholder:text-[#F39508]/[50%] font-sans relative w-full px-3 py-3 bg-[#FFE6C0] placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none sm:text-sm" placeholder={lang('password')} />
+                                            <input id="password-field" onChange={onChange(setPassword)} name="password" type="password" autoComplete="off" required className="appearance-none rounded pl-11 placeholder:text-[#F39508]/[50%] font-sans relative w-full px-3 py-3 bg-[#FFE6C0] placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none sm:text-sm" placeholder={lang('password')} />
                                         </div>
 
                                         <div className="flex items-center m-2 justify-between">
@@ -54,7 +95,17 @@ export default function Login() {
                                             </div>
                                         </div>
                                         <div className="mt-5 w-full flex pr-2 justify-end">
-                                            <button className="rounded bg-[#F39508] px-10 py-2 text-white">{lang('sign_in', 'upper')}</button>
+                                            <button onClick={()=> setLoading(!loading)} type="submit" className="rounded bg-[#F39508] disabled:opacity-70 transition duration-500 ease-in-out px-6 flex justify-center items-center py-2 text-white">
+                                                <div className="inline-flex gap-2">
+                                                    {loading && (
+                                                        <div className="bg-white justify-center duration-700 delay-150 transition ease-in-out items-center flex rounded-full w-5 h-5">
+                                                            <GoogleCircle fill="#F39508" className="animate-spin" size={16}/>
+                                                        </div>
+                                                    )}
+                                                    <span>{lang('sign_in', 'upper')}</span>
+                                                </div>
+                                                
+                                            </button>
                                         </div>
 
                                         <div className="flex justify-center mt-10 mb-5">
