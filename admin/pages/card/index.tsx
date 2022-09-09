1136477b730dfc96e07116d1328f44453cd22dfc
muhammad abdurrahman <rachman.sd@gmail.com>
2022-09-09 10:44:48 +0700

    feat: update

---
+++ admin/pages/card/index.tsx
@@ -0,0 +1,37 @@
+import MainLayout from '../../components/layout/MainLayout'
+import styles from '../../styles/Home.module.css'
+import { NextPageWithLayout } from '../_app'
+
+const Home: NextPageWithLayout = () => {
+  return (
+    <div className={styles.container}>
+
+      <main className={styles.main}>
+        <div>
+          under construction
+        </div>
+      </main>
+    </div>
+  )
+}
+
+Home.getLayout = function (page) {
+
+  const leftComponent = {
+    component: <h1 className=''>{ process.env.NEXT_PUBLIC_APP_NAME || "PERPUSTAKAAN" }</h1>,
+    mobileClassName: 'text-lg text-[#F39508]  font-bold font-jakarta-sans',
+    desktopClassName: 'text-[#F39508] text-2xl font-bold font-jakarta-sans'
+  }
+  return (<MainLayout both={leftComponent}>{page}</MainLayout>)
+}
+
+// export const getServerSideProps = async () => {
+//   // method post
+//   const res = await fetch(
+//     'https://api.github.com/repos/vercel/next.js', {
+//       method: 'POST',
+//     }
+//   )
+// }
+
+export default Home
