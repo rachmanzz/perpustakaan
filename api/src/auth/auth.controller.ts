3b00bb66854e03e96f4a39e265d6e5bba675ed50
muhammad abdurrahman <rachman.sd@gmail.com>
2022-09-09 10:47:27 +0700

    fix: Update auth controller

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,5 +1,8 @@
-import { Body, Controller, Post, Request,
+import { JwtAuthGuard } from '@common/guards/jwt-auth.guard';
+import { Body, Controller, Get, Post, Request,
   UseGuards, VERSION_NEUTRAL } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { retry } from 'rxjs';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { User, UserType } from '../users/entities/user.entity';
 import { AuthService } from './auth.service';
@@ -25,5 +28,12 @@ export class AuthController {
   @Post('login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
+  @Get('check')
+  async checkStatus (@Request() req) {
+    return req.user
   }
 }
